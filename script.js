f6cc62482834a577fd213b78006add0b957dedec
NoblePrinceii <farhan.begg@students.makeschool.com>
2021-02-25 17:27:13 -0800

    upate styles

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let countyData
 let educationData
 
 let canvas = d3.select('#canvas')
+let tooltip = d3.select('#tooltip')
 
 let drawMap = () => {
 
@@ -14,6 +15,54 @@ let drawMap = () => {
     .append('path')
     .attr('d', d3.geoPath())
     .attr('class', 'county')
+    .attr('fill', (countyDataItem) => {
+        let id = countyDataItem['id']
+        let county = educationData.find((item) => {
+            return item['fips'] === id
+        })
+        let percentage = county['bachelorsOrHigher']
+        if(percentage <= 15){
+            return 'tomato'
+        }else if (percentage <= 30){
+            return 'orange'
+        }else if (percentage <= 45){
+            return 'lightgreen'
+        }else {
+            return 'limegreen'
+        }
+    })
+    .attr('data-fips', (countyDataItem) => {
+        return countyDataItem['id']
+    })
+    .attr('data-education', (countyDataItem) => {
+        let id = countyDataItem['id']
+        let county = educationData.find((item) => {
+            return item['fips'] === id
+        })
+        let percentage = county['bachelorsOrHigher']
+        return percentage
+
+    })
+    .on('mouseover', (countyDataItem) => {
+        tooltip.transition()
+                .style('visibility', 'visible')
+    
+        let fips = countyDataItem['id']
+        let county = educationData.find((county) => {
+            return county['fips'] === fips
+        })
+    
+        tooltip.text(county['fips'] + ' - ' + county['area_name'] + ', ' + 
+            county['state'] + ' : ' + county['bachelorsOrHigher'] + '%')
+
+        tooltip.attr('data-education',county['bachelorsOrHigher'] )
+
+    })
+    .on('mouseout', (countyDataItem) => {
+        tooltip.transition()
+                .style('visibility', 'hidden')
+    })
+    
  
 }
 // converts url data into json object
